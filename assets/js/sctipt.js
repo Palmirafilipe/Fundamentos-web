b4d6f4a4f50e9488fd37bf5be54a116843f6576a
Palmira Filipe <palmirafilipe24@gmail.com>
2021-04-20 23:52:43 -0300

    faltando

---
+++ assets/js/sctipt.js
@@ -0,0 +1,32 @@
+
+
+/*
+case Sentive = reconhece letras maiusculas e minusculas
+
+por Tag: getElementByTagNome()
+por Id: getElementById()
+por Nome: getElementsByName()
+por Classe:getElementsByClassName()
+por Seletor: querySelector()
+*/
+
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto =  document.querySelector('#assunto')
+
+nome.style.width = '100%'
+email.style.width = '100%'
+
+function validaNome() {
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3){
+        txt.innerHTML = 'Nome Inválido'
+        txt.style.color = 'red'
+    }
+    else {
+        txt.innerHTML ='Nome Válido'
+        txt.style.color = 'green'
+    }
+}
+
+
