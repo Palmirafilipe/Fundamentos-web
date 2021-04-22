3be0ea2f0972fb17e3cd11e10802d1b89fd502d4
Palmira Filipe <palmirafilipe24@gmail.com>
2021-04-22 01:14:12 -0300

    java script

--- assets/js/sctipt.js
+++ assets/js/sctipt.js
@@ -15,18 +15,31 @@ let email = document.querySelector('#email')
 let assunto =  document.querySelector('#assunto')
 
 nome.style.width = '100%'
-email.style.width = '100%'
+email.style.width= '100%'
 
 function validaNome() {
-    let txtNome = document.querySelector('#txtNome')
-    if (nome.value.length < 3){
-        txt.innerHTML = 'Nome Inválido'
-        txt.style.color = 'red'
-    }
-    else {
-        txt.innerHTML ='Nome Válido'
-        txt.style.color = 'green'
-    }
+
+   let txtNome = document.querySelector('#txtNome')
+   if (nome.value.length < 3) {
+      txt.innerHTML = 'Nome Inválido'
+      txt.style.color = 'red' 
+   } else {
+      txt.innerHTML = 'Nome Válido'
+      txt.style.color = 'green'
+
+   }
+
 }
 
+function validaEmail() {
+   let txtEmail = document.querySelector('#txtEmail') 
 
+   if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ) {
+      txt.innerHTML = 'E-mail inválido'
+      txt.style.color = 'red'
+   } else {
+      txt.innerHTML = 'E-mail válido'
+      txt.style.color = 'green'
+     
+   }
+}
